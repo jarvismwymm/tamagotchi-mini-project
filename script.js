5767f25aa4ed7741c6f3ea2b74805dd6b5bb9832
jarviswynn123 <jwynn705@gmail.com>
2023-10-05 18:25:39 -0400

    Most of project functionality has been completed

--- script.js
+++ script.js
@@ -10,16 +10,31 @@ class Tamagotchi {
         this.age = 0;
         this.hungerInterval = setInterval(() => {
             this.hunger++
-            displayHunger()
+            displayHunger();
+            this.areYouDead()
         }, 2000)
         this.sleepinessInterval = setInterval(() => {
             this.sleepiness++
             displaySleepiness()
+            this.areYouDead()
         }, 2000)
         this.boredomInterval = setInterval(() => {
             this.boredom++
             displayBoredom()
+            this.areYouDead()
         }, 2000)
+        this.ageInterval = setInterval(() => {
+            this.age++
+            displayAge()
+            if (this.age == 3) {
+                let element3 = document.getElementById("Tamagotchi")
+            element3.src = "./download.png"
+            }
+            if (this.age == 5) {
+                let element3 = document.getElementById("Tamagotchi")
+            element3.src = "./mametchi.jpg"
+            }
+        }, 4000)
         
     }
     addName(name) {
@@ -34,6 +49,15 @@ class Tamagotchi {
     play() {
         this.boredom -= 1
     }
+    areYouDead() {
+        if (this.hunger == 10 || this.sleepiness == 10 || this.boredom == 10) {
+            clearInterval(this.hungerInterval)
+            clearInterval(this.sleepinessInterval)
+            clearInterval(this.boredomInterval)
+            clearInterval(this.ageInterval)
+            displayDeath()
+        }
+    }
 }
 
 
@@ -46,7 +70,14 @@ function start() {
      displayHunger();
      displaySleepiness();
      displayBoredom();
+     displayAge();
      tamagotchi1.addName(response)
+     let element = document.getElementById("death-message")
+    element.innerHTML = ""
+     let element1 = document.getElementById("display-name")
+    element1.innerHTML = response
+    let element3 = document.getElementById("Tamagotchi")
+    element3.src = "./violetchi.jpg"
      console.log(tamagotchi1)
 }
 
@@ -57,6 +88,7 @@ function feed() {
 function sleep() {
     tamagotchi1.goToSleep() 
     displaySleepiness()
+    
 }
 function play() {
     tamagotchi1.play() 
@@ -76,4 +108,13 @@ function displaySleepiness() {
 function displayBoredom() {
     let element = document.getElementById("boredom-display")
     element.innerHTML = tamagotchi1.boredom
+}
+function displayAge() {
+    let element = document.getElementById("age-display")
+    element.innerHTML = tamagotchi1.age
+}
+
+function displayDeath() {
+    let element = document.getElementById("death-message")
+    element.innerHTML = `You neglected ${tamagotchi1.name}. ${tamagotchi1.name} has died`
 }
